251c83b85cb9d392e40462b05b145937ae534438
soyelmike <miguelsistemas18@gmail.com>
2025-07-18 11:47:14 -0600

    menu de inicio prueba web

---
+++ menu_inicio.php
@@ -0,0 +1,56 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OPDAPAS</title>
+
+    <!--
+                                             _        _                _                              _ 
+                                            (_)      (_)              | |                            | |
+ _ __    __ _   __ _   ______   _ __   _ __  _   ___  _  _ __    __ _ | |  ______    ___   _ __    __| |
+| '_ \  / _` | / _` | |______| | '_ \ | '__|| | / __|| || '_ \  / _` || | |______|  / _ \ | '_ \  / _` |
+| |_) || (_| || (_| |          | |_) || |   | || (__ | || |_) || (_| || |          | (_) || |_) || (_| |
+| .__/  \__,_| \__, |          | .__/ |_|   |_| \___||_|| .__/  \__,_||_|           \___/ | .__/  \__,_|
+| |             __/ |          | |                      | |                               | |           
+|_|            |___/           |_|                      |_|                               |_|            doom
+
+ESTA PAGINA ES EL MENU DE INICIO DEL SISTEMA PARA PODER NAVEGAR POR LAS DIFERENTES CATEGORIAS 
+-->
+
+    <Table  width="100%" height ="600">
+        <tr><td  height="90" colspan="2" align="center" bgcolor="fbd5c5"> <h1>OPDAPAS IXTAPAN DE LA SAL </h1></td></tr>
+
+<!--ESTE LINK TE REDIRECCIONA A SCRIPT "reporte_test.php" PAR GENERAR REPORTES -->
+<tr>
+    <td bgcolor="fbd5c5" height="90"> <a href="reporte.php" target="_blank">REPORTES DE OPERACION </a> </td> <td rowspan ="6" align="center"><img src="logos/PORTADA.png" ></td>
+</tr>
+
+
+
+<!-- ESTE LINK TE REDIRECCIONA AL SCRIPT "buscar_empleado.php" PARA GENERAR FORMATOS PARA DESCANSOS -->
+<tr>
+    <td bgcolor="fbd5c5" height="90"> <a href="buscar_empleado.php" target="_blank">FORMATO DE PERMISOS </a></td>
+</tr>
+<tr>
+    <td bgcolor="fbd5c5" height="90">REPORTES EN BLANCO</td>
+</tr>
+<tr>
+    <td bgcolor="fbd5c5" height="90"></td>
+</tr>
+<tr>
+    <td bgcolor="fbd5c5" height="90"></td>
+</tr>
+<tr>
+    <td bgcolor="fbd5c5" height="90"></td>
+</tr>
+    </Table>
+
+</head>
+<body>
+
+
+</body>
+</html>
